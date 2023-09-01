dd4d356abd34c1cc211fdb378915fd0c519562bc
azizul96 <alifalamin42@gmail.com>
2023-09-02 04:26:58 +0600

    blog added

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+let currentActiveID = 1000
+document.getElementById('sort-by-view').addEventListener('click', function(){
+    console.log(currentActiveID);
+    fetch(`https://openapi.programming-hero.com/api/videos/category/${currentActiveID}`)
+    .then(res=>res.json())
+    .then(resData => {
+        const cards = resData.data
+        cards.sort((a,b) => parseFloat(b.others.views) - parseFloat(a.others.views)) 
+        handleLoadCard(cards)
+    })
+})
+const activeCategory =(catID) =>{
+    currentActiveID = parseInt(catID)
+
+}
+
+
 const loadCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await response.json()
@@ -10,24 +27,27 @@ const loadCategory = async () => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div>
-            <button onclick="handleLoadCard('${singleCategory.category_id}')" class="btn  px-5 py-3 bg-[#FF1F3D] text-white  ">${singleCategory.category}</button>
+            <button onclick="getCard('${singleCategory.category_id}');activeCategory('${singleCategory.category_id}')" class="btn  px-5 py-3 bg-[#FF1F3D] text-white  ">${singleCategory.category}</button>
         </div>
         `;
         tabContainer.appendChild(div);
+        
     });
 }
-let cards
-const handleLoadCard = async (categoryID) =>{
+const getCard = async(categoryID)=> {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryID}`)
     const data = await response.json()
-    // console.log(data);
-    cards = data.data
-    // sortByViews()
+    const cards = data.data
+    handleLoadCard(cards)
+}
+
+const handleLoadCard = async (cards) =>{
+    
     const cardContainer = document.getElementById("card-container")
     cardContainer.textContent =''
     const noDataContainer = document.getElementById("no-data-card")
     noDataContainer.textContent =''
-    if(categoryID === "1005"){
+    if(cards.length === 0){
         
         const div = document.createElement('div')
         div.innerHTML = `
@@ -61,27 +81,28 @@ const handleLoadCard = async (categoryID) =>{
         }
 
         div.innerHTML = `
-        <div class="card">
-                <figure class="rounded-md">
-                    <div class="relative">
-                        <img src="${card.thumbnail}" alt="Shoes" class=" rounded-md md:w-[320px] md:h-[200px]" />
-                        ${time}
+
+        <div class="card   ">
+        <figure class="rounded-md relative">
+            <img src="${card.thumbnail}" alt="Shoes" class=" w-full h-56"/>
+            ${time}
+        </figure>
+        <div class="card-body  rounded-md px-0">
+            <div class="flex gap-3 justify-start">
+                <div>
+                    <img src="${card.authors[0].profile_picture}" alt="" class="w-10 h-10 rounded-full">
+                </div>
+                <div >
+                    <h2 class="font-bold text-base text-[#171717] mb-1">${card.title}</h2>
+                    <div class="flex  flex-row justify-start gap-2 mb-3">
+                    <p class="font-normal text-sm flex-grow-0">${card.authors[0].profile_name}</p>
+                    <p>${card.authors[0].verified? ` <img src="blue.svg"/>`: ''}</p>
                     </div>
-                </figure>
-                <div class=" flex gap-3 justify-start mt-5 px-3 ">
-                    <div>
-                        <img src="${card.authors[0].profile_picture}" alt="" class="w-10 h-10 rounded-full">
-                    </div>
-                    <div >
-                        <h2 class="font-bold text-base text-[#171717] mb-1">${card.title}</h2>
-                        <div class="flex flex-row justify-start gap-1 mb-3">
-                        <p class="font-normal text-sm">${card.authors[0].profile_name}</p>
-                        <p>${card.authors[0].verified? ` <img src="blue.svg"/>`: ''}</p>
-                        </div>
-                        <p class="font-normal text-sm">${card.others.views} views</p>
-                    </div>
+                    <p class="font-normal text-sm ">${card.others.views} views</p>
                 </div>
             </div>
+        </div>
+      </div>
         `
         cardContainer.appendChild(div)
     })
@@ -89,15 +110,13 @@ const handleLoadCard = async (categoryID) =>{
 }
 
 loadCategory()
-handleLoadCard(categoryID = 1000)
+getCard(1000)
 
 
 
 
-const sortByViews = ()=>{
-    cards.sort((a,b) => parseFloat(b.others.views)- parseFloat(a.others.views)) 
-    
-}
+
+// cards.sort((a,b) => parseFloat(b.others.views)- parseFloat(a.others.views)) 
 
 const blogHandler =()=>{
     window.location.href ='blog.html'
