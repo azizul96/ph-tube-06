a6ddb1e1278da9f70fde54ac9806402393a6714f
azizul96 <alifalamin42@gmail.com>
2023-09-01 17:03:44 +0600

    time added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 const loadCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await response.json()
     const categories = data.data
+    
     
     const tabContainer = document.getElementById('tab-container');
     categories.forEach(singleCategory => {
@@ -14,26 +16,22 @@ const loadCategory = async () => {
         tabContainer.appendChild(div);
     });
 
-
 }
-
 const handleLoadCard = async (categoryID) =>{
-    
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryID}`)
     const data = await response.json()
-    
     const cards = data.data
     const cardContainer = document.getElementById("card-container")
     cardContainer.textContent =''
     const noDataContainer = document.getElementById("no-data-card")
     noDataContainer.textContent =''
-    if(cards.length == 0){
+    if(cards.length === 0){
         
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card  ">
             <figure class="px-10 pt-10">
-                <img src="./image/Icon.png" alt="Shoes" class="rounded-xl" />
+                <img src="./image/Icon.png" alt="" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
                 <h2 class="card-title">Oops!! Sorry, There is no <br>content here</h2>
@@ -44,15 +42,28 @@ const handleLoadCard = async (categoryID) =>{
     }
     else{
     cards.forEach(card =>{
-        console.log(card);
-        const div = document.createElement('div')
+        const div = document.createElement('div');
+        const second = `${card.others.posted_date}`
+        let h= parseFloat(second)/3600;
+        let m=parseFloat(second)%60;
+        let hm;
+        let time;
+        if(second>0&&m>0)
+        {
+             hm =parseInt(h)+" "+"hrs"+" "+parseInt(m)+" "+"min"+" "+"ago";
+             time =` <p class="absolute bg-[#171717] text-white bottom-2 right-2 px-2 rounded-md">${hm}</p>`
+        }
+        else{
+            hm=''
+            time='';
+        }
 
         div.innerHTML = `
         <div class="card">
                 <figure class="rounded-md">
                     <div class="relative">
                         <img src="${card.thumbnail}" alt="Shoes" class=" rounded-md md:w-[320px] md:h-[200px]" />
-                        <p class="absolute bg-[#171717] text-white bottom-2 right-2 px-2 rounded-md">3hrs 56 min ago</p>
+                        ${time}
                     </div>
                 </figure>
                 <div class=" flex gap-3 justify-start mt-5 px-3 ">
@@ -61,7 +72,10 @@ const handleLoadCard = async (categoryID) =>{
                     </div>
                     <div >
                         <h2 class="font-bold text-base text-[#171717] mb-1">${card.title}</h2>
-                        <p class="font-normal text-sm mb-3">${card.authors[0].profile_name} <i class="fa-solid fa-certificate"></i></p>
+                        <div class="flex flex-row justify-start gap-1 mb-3">
+                        <p class="font-normal text-sm">${card.authors[0].profile_name}</p>
+                        <p>${card.authors[0].verified? ` <img src="blue.svg"/>`: ''}</p>
+                        </div>
                         <p class="font-normal text-sm">${card.others.views} views</p>
                     </div>
                 </div>
@@ -70,6 +84,10 @@ const handleLoadCard = async (categoryID) =>{
         cardContainer.appendChild(div)
     })
     }
+    sortByViews(cards)
+}
+
+const sortByViews = (cards)=>{
     
 }
 
